d347fd4af64a717a247bdcbae9835f3c5bf41350
Qwe Qwe <qwe@qwe.qwe>
2021-02-11 19:16:21 +0700

    Add multi-layer support to link ops

--- Link Active Cel.lua
+++ Link Active Cel.lua
@@ -8,22 +8,24 @@ end
 
 
 app.transaction( function()
-    local sprite, layer, cel = app.activeSprite, app.activeLayer, app.activeCel
+    local sprite, frame = app.activeSprite, app.activeCel.frameNumber
 
-    -- For some reason, in the script, linking skips empty cels (due to how range works i guess?)
-    -- let's create empties where they should go
-    for _,frame in ipairs(app.range.frames) do
-        if not layer:cel(frame) then
-            sprite:newCel(layer, frame)
+    for k,layer in ipairs(app.range.layers) do
+        -- For some reason, in the script, linking skips empty cels (due to how range works i guess?)
+        -- let's create empties where they should go
+        for _,frame in ipairs(app.range.frames) do
+            if not layer:cel(frame) then
+                sprite:newCel(layer, frame)
+            end
         end
+        
+        -- we need to link to the active cel, but the app.command links to the first cel, so  let's copy the image to the first
+        local proto = layer:cel(app.range.frames[1])
+        local cel = layer:cel(frame)
+        proto.image = cel.image
+        proto.position = cel.position
+        -- ??? not copying cel.color, cel.opacity etc. is it needed ???
     end
-    
-    -- we need to link to the active cel, but the app.command links to the first cel, so  let's copy the image to the first
-    local proto = layer:cel(app.range.frames[1])
-
-    proto.image = cel.image
-    proto.position = cel.position
-    -- ??? not copying cel.color, cel.opacity etc. is it needed ???
 
     app.command.LinkCels()
 end)
--- Link Cel To All Frames.lua
+++ Link Cel To All Frames.lua
@@ -7,27 +7,40 @@ if not app.activeCel then
 end
 
 app.transaction( function()
-    local sprite, layer, cel = app.activeSprite, app.activeLayer, app.activeCel
+    local layers = {}
+    local sprite, frame = app.activeSprite, app.activeCel.frameNumber
     local cels_row = {}
 
     for i,cel in ipairs(sprite.frames) do
-        -- For some reason, in the script, linking skips empty cels (due to how range works i guess?)
-        -- let's create empties where they should go
-        if not layer:cel(i) then
-            cel = sprite:newCel(layer, i)
-        end
         cels_row[i] = i
     end
 
-    -- we need to link to the active cel, but the app.command links to the first cel, so  let's copy the image to the first
-    local proto = layer:cel(1)
-    proto.image = cel.image
-    proto.position = cel.position
-    -- ??? not copying cel.color, cel.opacity etc. is it needed ???
+    -- stash layer references, as range.clean will be called later
+    for i,layer in ipairs(app.range.layers) do
+        table.insert(layers, layer)
+    end
 
-    app.range:clear()
-    app.range.layers = {app.activeLayer}
-    app.range.frames = cels_row
-    app.command.LinkCels()
-    app.range:clear()
+    for k,layer in ipairs(layers) do
+        
+        -- For some reason, in the script, linking skips empty cels (due to how range works i guess?)
+        -- let's create empties where they should go
+        for i,cel in ipairs(sprite.frames) do
+            if not layer:cel(i) then
+                cel = sprite:newCel(layer, i)
+            end
+        end
+
+        -- we need to link to the active cel, but the app.command links to the first cel, so  let's copy the image to the first
+        local proto = layer:cel(1)
+        local cel = layer:cel(frame)
+        proto.image = cel.image
+        proto.position = cel.position
+        -- ??? not copying cel.color, cel.opacity etc. is it needed ???
+
+        app.range:clear()
+        app.range.layers = {layer}
+        app.range.frames = cels_row
+        app.command.LinkCels()
+        app.range:clear()
+    end
 end)
