1c1ce4319cce30eab23a4d888f85aecb3cf31cf1
Qwe Qwe <qwe@qwe.qwe>
2021-02-15 15:28:32 +0700

    Add settings menu

--- Commands.lua
+++ Commands.lua
@@ -1,84 +1,111 @@
 function run_script(f) 
+    local s = app.fs.joinPath(app.fs.userConfigPath, "extensions", "shurushki", f) .. ".lua"
+
     return function()
-        dofile(app.fs.joinPath(app.fs.userConfigPath, "extensions", "shurushki", f))
+        dofile(s)
     end
 end
 
+
+-- create a copy of a table when needed
+local function all_commands()
+    return {
+        { name="Bitmap Text", group="edit_fill", location="Edit > Bitmap Text" },
+        { name="Export Atlas", group="file_export", location="File > Export Atlas" },
+        { name="Frame Pattern", group="select_simple", location="Select > Frame Pattern" },
+        { name="Gamma Gradient", group="palette_generation", location="Color Bar > Options > Gamma Gradient" },
+        { name="Highlight Cels", group="view_animation_helpers", location="View > Highlight Layers" },
+        { name="Highlight Layers", group="view_animation_helpers", location="View > Highlight Layers" },
+        { name="Link Active", group="cel_popup_links", location="Timeline > Cel > Right Click" },
+        { name="Link To All Frames", group="cel_popup_links", location="Timeline > Cel > Right Click"},
+        { name="Move Cels", group="edit_transform", location="Edit > Move Cels" },
+        { name="Splice Animation", group="file_export", location="File > Splice Animation"},
+        { name="Time Stretch", group="cel_frames", location="Frame > Time Stretch" }
+    }
+end
+-- a copy to use as a reference
+local commands = all_commands()
+
+
+local function settings_dialog(prefs, firsttime)
+    -- prepare and show dialog
+    local dlg = Dialog()
+
+    dlg:separator{ text="Shurushki Settings" }
+    if firsttime then
+        dlg:label({text="FIRST TIME SETUP"})
+        dlg:newrow()
+        dlg:label({text="Thank you for using the plugin! Hope it comes in useful."})
+        dlg:separator()
+    end
+    dlg:label{ text="Choose features you want to use" }
+    dlg:newrow()
+    dlg:label{ text="Please check out readme file for an explanation of features" }
+
+    for _,prop in ipairs(commands) do
+        local enabled = false
+        for _,p in ipairs(prefs.commands) do
+            if p.name == prop.name then
+                enabled = true
+                break
+            end
+        end
+
+        dlg:check{ id=prop.name, label=prop.name, selected=enabled, text=prop.location }
+    end
+
+    dlg:button{id="enable_all", text="Enable All"}
+    dlg:button{id="disable_all", text="Disable All"}
+    dlg:button{id="ok", text="OK"}
+    if not firsttime then
+        dlg:button{id="cancel", text="Cancel"}
+    end
+
+    dlg:show()
+
+    -- apply changes
+
+    if dlg.data.cancel then
+        return
+    elseif dlg.data.disable_all then 
+        prefs.commands = {}
+    elseif dlg.data.enable_all then
+        prefs.commands = all_commands()
+    else
+        prefs.commands = {}
+        for _,prop in ipairs(commands) do 
+            if dlg.data[prop.name] then
+                table.insert(prefs.commands, prop)
+            end
+        end
+    end
+
+    -- be loud
+    if not firsttime then
+        app.alert("Menus will update the next time Aseprite starts.")
+    end
+end
+
+
 function init(plugin)
-    plugin:newCommand{
-        id="Bitmap Text",
-        title="Bitmap Text",
-        group="edit_fill",
-        onclick=run_script("Bitmap Text.lua")
-    }
+    if not plugin.preferences.commands then 
+        plugin.preferences.commands = all_commands()
+        settings_dialog(plugin.preferences, true)
+    end
+
+    for _,props in ipairs(plugin.preferences.commands) do
+        plugin:newCommand{
+            id=props.name,
+            title=props.name,
+            group=props.group,
+            onclick=run_script(props.name)
+        }
+    end
 
     plugin:newCommand{
-        id="Export Atlas",
-        title="Export Atlas",
-        group="file_export",
-        onclick=run_script("Export Atlas.lua")
-    }
-
-    plugin:newCommand{
-        id="Frame Pattern",
-        title="Frame Pattern",
-        group="select_simple",
-        onclick=run_script("Pattern Select Frames.lua")
-    }
-
-    plugin:newCommand{
-        id="Gamma Gradient",
-        title="Gamma Gradient",
-        group="palette_generation",
-        onclick=run_script("Gamma Gradient.lua")
-    }
-
-    plugin:newCommand{
-        id="Highlight Cels",
-        title="Highlight Cels",
-        group="view_animation_helpers",
-        onclick=run_script("Highlight Cels.lua")
-    }
-
-    plugin:newCommand{
-        id="Highlight Layers",
-        title="Highlight Layers",
-        group="view_animation_helpers",
-        onclick=run_script("Highlight Layers.lua")
-    }
-
-    plugin:newCommand{
-        id="Link Active Cel",
-        title="Link Active",
-        group="cel_popup_links",
-        onclick=run_script("Link Active Cel.lua")
-    }
-
-    plugin:newCommand{
-        id="Link Cel To All Frames",
-        title="Link To All Frames",
-        group="cel_popup_links",
-        onclick=run_script("Link Cel To All Frames.lua")
-    }
-
-    plugin:newCommand{
-        id="Move Cels",
-        title="Move Cels...",
-        group="edit_transform",
-        onclick=run_script("Move Cels.lua")
-    }
-
-    plugin:newCommand{
-        id="Splice Animation",
-        title="Splice Animation",
-        group="file_export",
-        onclick=run_script("Splice Animation.lua")
-    }
-
-    plugin:newCommand{
-        id="Time Stretch",
-        title="Time Stretch",
-        group="cel_frames",
-        onclick=run_script("Time Stretch.lua")
+        id="Shurushki",
+        title="Shurushki",
+        group="help_readme",
+        onclick=function() settings_dialog(plugin.preferences) end
     }
 end
--- Readme.md
+++ Readme.md
@@ -9,46 +9,54 @@ A collection of badly maintained scripts for Aseprite with no unified purpose.
 1. Alternatively, in Aseprite, got to "Edit > Preferences > Extensions > Add Extension" and select the saved file.
 1. Might need to restart Aseprite for menu options to appear.
 
+## **Settings**
+
+If something goes wrong, it might help to delete `__pref.lua` file in the installation folder. It can be found by going to "Edit > Preferences > Extensions", picking **this** extension and clicking "Open Folder" nearby.
+
 ## **Scripts**
 
-## Bitmap Text (`Edit > Bitmap Text`)
+### Bitmap Text (`Edit > Bitmap Text`)
 
 Draw bitmap text using a simple bmf format used in ProMotion/GrafX2/Love2D/etc. Not to be confused with Angelcode's bmfont. An example of such font (by me) is included. Unlike ttf's, these fonts never blur, support color, and are pretty trivial to edit.
 
-## Export Atlas (`File > Export Atlas`)
+### Export Atlas (`File > Export Atlas`)
 
 Combine multiple sprites to a single texture and generates .json data for the layout. Similar to spritesheet export, but can combine multiple sprites, and each layer and frame is treated separately.
 **Requires** Python 3.x to be installed, along with `rectpack` package (can be installed as `python -m pip install rectpack`)
 
-## Highlight Cels (`View > Highlight Cels`)
+### Splice animation (`File > Splice Animation`)
+
+Export an animation by joining tags or frames several times.
+
+### Highlight Cels (`View > Highlight Cels`)
 
 Makes Timeline a bit easier to navigate by finding and highlighting frames of certain duration. Also allows to highlight cels manually, even though Aseprite now added its own tools for doing so.
 
 
-## Highlight Layers (`View > Highlight Layers`)
+### Highlight Layers (`View > Highlight Layers`)
 ![example](readme_files/layers.png)
 
 Highlights layers that use blending modes. Only applies to current/selected layers. You'll probably want to change the colors - mind that ase uses 0xAABBGGRR layout for colors, so e.g 0xff0044aa becomes opaque brown
 
 
-## Link Active Cel / Link To All Frames (`Timeline > Cel > Right Click`)
+### Link Active Cel / Link To All Frames (`Timeline > Cel > Right Click`)
 
 Make the current cel linked to either the selection, or the whole timeline. Unlike the default commands, all new cels store the same frame as the current cel, not the first one. Makes working with multiple layers for the background in an animation a bit faster.
 
-## Pattern Select (`Select > Frame Pattern`)
+### Pattern Select (`Select > Frame Pattern`)
 
 Select frames in a checkered order. Primary use is for dropping the frames from gifs to reduce the size (in that case, don't forget to adjust the framerate afterwards!).
 
-## Time Stretch (`Frame > Time Stretch`)
+### Time Stretch (`Frame > Time Stretch`)
 
 Speed up/slow down the animation proportionally by scaling frame durations, or adding/removing a constant amount of time to them.
 
-## Move Cels (`Edit > Move Cels`)
+### Move Cels (`Edit > Move Cels`)
 
 Move cels with a formula based on their frame time or frame number. Makes working with movement animations a little easier. 
 *Example: setting `x=6 * t` will move the sprite with the speed of 6 pixels per frame. After that, calling with `x=-6 * t` does the same but to the left, thus returns the sprite to marching at place.*
 
-## Gamma Gradient (`Palette Options > Gamma Gradient`)
+### Gamma Gradient (`Color Bar > Options > Gamma Gradient`)
 ![example](readme_files/gamma.png)
 
 Generates a gradient ramp from selected colors using the equation for overlapping light.
